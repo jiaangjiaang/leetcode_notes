41aafe3c782f53129a1d2e14f88bee035cc2aa0c
jiaangjiaang <1394964078@qq.com>
2022-06-20 17:57:19 +0800

    add a new soultion

--- 两数相加/sum_Two_Num.py
+++ 两数相加/sum_Two_Num.py
@@ -1,7 +1,5 @@
-from operator import le
+
 from typing import List
-
-
 """
 两层暴力循环，太慢，太占内存。
 class Solution:
@@ -12,9 +10,13 @@ class Solution:
                     return[i,j]
 """
 
-
-
-
+#新的方法
+class Solution:
+    def twoSum(nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            num = target - nums[i]
+            if num in nums[:i]:
+                return [i, nums.index(num)]
 
 
 if __name__ == "__main__":
